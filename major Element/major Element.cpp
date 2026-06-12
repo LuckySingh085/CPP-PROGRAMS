a27154ceb96cbe4fc5d612b23ac8683d21407302
LuckySingh085 <085luckysingh@gmail.com>
2026-06-12 04:41:14 +0000

    Jun 12, 2026, 10:11 AM

--- major Element/major Element.cpp
+++ major Element/major Element.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+int majorityElement(vector<int>& nums) {
+    int el = 0, freq = 0;
+    for (int val : nums) {
+        if (freq == 0) el = val;
+        freq += (val == el) ? 1 : -1;
+    }
+    return el;
+}
+
+int main() {
+    vector<int> nums = {2,2,1,1,1,2,2};
+    cout << majorityElement(nums);
+    return 0;
+}
