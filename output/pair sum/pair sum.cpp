7318aa1e607ef6cb1a3a589ab08e46aea4ad1b3c
LuckySingh085 <085luckysingh@gmail.com>
2026-06-12 04:23:27 +0000

    Jun 12, 2026, 9:53 AM

---
+++ output/pair sum/pair sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 4, 6, 8, 10};
+    int n = 5;
+    int target = 14;
+
+    for(int i=0; i<n; i++) {
+        for(int j=i+1; j<n; j++) {
+            if(arr[i] + arr[j] == target) {
+                cout << arr[i] << " + " << arr[j] << " = " << target << endl;
+            }
+        }
+    }
+
+    return 0;
+}
