9687ab1a30855ff70d6a2838211d1d4f53c04662
LuckySingh085 <085luckysingh@gmail.com>
2026-06-18 04:50:59 +0000

    Jun 18, 2026, 10:20 AM

--- pointer/pointer.cpp
+++ pointer/pointer.cpp
@@ -2,11 +2,11 @@
 #include<vector>
 using namespace std;
 int main(){
-    int a=10;
-    int*ptr=&a;
-    cout<<"value of a is"<<a<<endl;
-    cout<<"address of a is"<<&a<<endl;
-    cout<<"value of ptr is"<<ptr<<endl;
+    int a= 10;
+    int*ptr= &a;
+    cout<<"value of a is "<<a<<endl;
+    cout<<"address of a is "<<&a<<endl;
+    cout<<"value of ptr is "<<ptr<<endl;
     cout<<"value at address stored in ptr is"<<*ptr<<endl;
     return 0;
 }
