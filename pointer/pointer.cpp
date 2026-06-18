f8388325465a27d50d0d3d138ac94ffe394dc9e1
LuckySingh085 <085luckysingh@gmail.com>
2026-06-18 04:48:34 +0000

    Jun 18, 2026, 10:18 AM

--- pointer/pointer.cpp
+++ pointer/pointer.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int main(){
     int a=10;
     int*ptr=&a;
-    int**Parptr=&ptr;
-    cout<<&ptr<<endl;
-    cout<<Parptr<<endl;
+    cout<<"value of a is"<<a<<endl;
+    cout<<"address of a is"<<&a<<endl;
+    cout<<"value of ptr is"<<ptr<<endl;
+    cout<<"value at address stored in ptr is"<<*ptr<<endl;
     return 0;
 }
