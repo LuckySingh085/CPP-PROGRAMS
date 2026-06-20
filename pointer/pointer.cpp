88d799587948735f508249787831bb67865a04a6
LuckySingh085 <085luckysingh@gmail.com>
2026-06-20 03:44:34 +0000

    Jun 20, 2026, 9:14 AM

--- pointer/pointer.cpp
+++ pointer/pointer.cpp
@@ -6,7 +6,7 @@ int main(){
     int*ptr= &a;
     cout<<"value of a is "<<a<<endl;
     cout<<"address of a is "<<&a<<endl;
-    cout<<"value of ptr is "<<ptr<<endl;
-    cout<<"value at address stored in ptr is"<<*ptr<<endl;
+    cout<<"value of ptr is "<<ptr<<endl;//jab bina * ke print akru gii tb address print kare gaa 
+    cout<<"value at address stored in ptr is"<<*ptr<<endl;//* use kare gye to value print hoga
     return 0;
 }
